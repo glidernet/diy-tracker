de51075ac6e8d9b4d2f1ea52adc5e057ad274ccb
Pawel Jalocha <Pawel.Jalocha@gmail.com>
2015-06-23 10:43:28 +0100

    Knob support code

---
+++ knob.cpp
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+
+#include <FreeRTOS.h>
+#include <task.h>
+#include <semphr.h>
+#include <queue.h>
+
+#include "knob.h"
+
+#include "format.h"
+#include "uart1.h"                         // console UART
+#include "adc.h"
+
+#include "main.h"
+
+volatile uint8_t KNOB_Tick=8;
+
+#ifdef __cplusplus
+  extern "C"
+#endif
+void vTaskKNOB(void* pvParameters)
+{ // ADC_Configuration();
+
+  uint8_t Tick=KNOB_Tick;
+  for( ; ; )
+  { vTaskDelay(40);
+
+    uint16_t Knob     = ADC1_Read(ADC_Channel_8);
+    uint16_t PrevKnob = ((uint16_t)Tick<<8)+0x80;
+     int16_t Err      = Knob-PrevKnob;
+    if(abs(Err)>=(0x80+0x20))                              // 0x20 is the histeresis to avoid noisy input
+    { KNOB_Tick = (Tick = (Knob>>8)); Play(0x40+Tick, 10);
+      // xSemaphoreTake(UART1_Mutex, portMAX_DELAY);
+      // Format_UnsDec(UART1_Write, (uint16_t)Tick);
+      // UART1_Write('\r'); UART1_Write('\n');
+      // xSemaphoreGive(UART1_Mutex);
+    }
+
+    // uint16_t MCU_Temp = ADC1_Read(ADC_Channel_TempSensor);
+    // uint16_t MCU_Vref = ADC1_Read(ADC_Channel_Vrefint);
+
+  }
+
+}
---
+++ knob.h
@@ -0,0 +1,8 @@
+extern volatile uint8_t KNOB_Tick;
+
+#ifdef __cplusplus
+  extern "C"
+#endif
+void vTaskKNOB(void* pvParameters);
+
+
