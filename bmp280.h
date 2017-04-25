7d46e4a51b5a76cf27e9106f5aaae9035f0ee764
Pawel Jalocha <Pawel.Jalocha@gmail.com>
2017-04-25 10:34:57 +0200

    support not complete yet

--- bmp280.h
+++ bmp280.h
@@ -21,10 +21,6 @@ class BMP280
    static const uint8_t REG_CTRL     = 0xF4; // control: TTTPPPMM TTT=temp. oversampling, PPP=pressure oversampling, MM=power mode
    static const uint8_t REG_CONFIG   = 0xF5; // config: TTTFFF_S TTT=? FFF=Filter S=3-wire SPI
 
-   static const uint8_t MEAS_TEMP    = 0x2E; // measure temperature
-   static const uint8_t MEAS_PRESS   = 0x34; // measure pressure
-   static const uint8_t MEAS_BUSY    = 0x20; // measurement-busy flag
-
    static const uint8_t REG_PRESS      = 0xF7; // Pressure result:
    static const uint8_t REG_PRESS_MSB  = 0xF7; // Pressure result: MSB
    static const uint8_t REG_PRESS_LSB  = 0xF8; // Pressure result: LSB
@@ -72,6 +68,54 @@ class BMP280
     P8  = -14600;
     P9  =   6000;
     D   =      0; }
+#endif
+
+#ifndef NO_RTOS
+  uint8_t CheckID(void) // check ID, to make sure the BMP180 is connected and works correctly
+   { uint8_t ID;
+     Error=I2C_Read(Bus, ADDR, REG_ID, ID); if(Error) return Error;
+     return ID!=0x58; } // 0 => no error and correct ID
+
+  uint8_t ReadCalib(void) // read the calibration constants from the EEPROM
+  { Error=I2C_Read(Bus, ADDR, REG_CALIB, (uint8_t *)(&T1), 26);
+    return Error; }
+
+  uint8_t ReadBusy(void) // check if temperature and pressure conversion is done
+  { uint8_t Status;
+    Error=I2C_Read(Bus, ADDR, REG_STATUS, Status); if(Error) return Error;
+    return (Status&0x09)==0; } // 1 = busy, 0 => ready
+
+  uint8_t WaitReady(uint8_t Timeout=20, uint8_t Wait=30) // wait for the conversion to be ready
+  { vTaskDelay(Wait);
+    for(; Timeout; Timeout--)
+    { uint8_t Err=ReadBusy(); if(Err>1) return Err;
+      if(Err==0) return 0;
+      vTaskDelay(1); }
+    return 0xFF; }
+
+  uint8_t Trigger(void)                                    // start a temperature+pressure measurement
+  { Error=I2C_Write(Bus, ADDR, REG_CONFIG, 0x00); if(Error) return Error;
+    Error=I2C_Write(Bus, ADDR, REG_CTRL,   0x55); return Error; } // P.osp=16x, T.osp=2x
+
+  uint8_t ReadRawPress(void)
+  { RawPress=0;
+    Error=I2C_Read(Bus, ADDR, REG_PRESS, (uint8_t *)(&RawPress), 3); if(Error) return Error;
+    RawPress = ((RawPress<<16)&0xFF0000) | (RawPress&0x00FF00) | ((RawPress>>16)&0x0000FF);
+    RawPress>>=4;
+    return 0; }
+
+  uint8_t ReadRawTemp(void)
+  { RawTemp=0;
+    Error=I2C_Read(Bus, ADDR, REG_TEMP, (uint8_t *)(&RawTemp), 3); if(Error) return Error;
+    RawTemp = ((RawTemp<<16)&0xFF0000) | (RawTemp&0x00FF00) | ((RawTemp>>16)&0x0000FF);
+    RawTemp>>=4;
+    return 0; }
+
+  uint8_t Acquire(void)
+  { if(Trigger) return Error;
+    if(WaitReady()) return Error;
+    if(ReadRawTemp()) return Error;
+    return ReadRawPree(); }
 #endif
 
   void CalcTemperature(void)   // calculate the temperature from raw readout and calibration values
