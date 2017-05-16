70544d73ab9ce9ea27655a26e17b0ef465e111a1
Pawel Jalocha <Pawel.Jalocha@gmail.com>
2017-05-16 21:50:54 +0100

    A bit more extensive dump of numbers

--- freqplan_test.cc
+++ freqplan_test.cc
@@ -9,13 +9,13 @@ int main(int argc, char *argv[])
   FreqPlan Plan;
   Plan.setPlan(2);
 
-  for(int Ofs=0; Ofs<32; Ofs++)
+  for(int Ofs=0; Ofs<10000; Ofs++)
   { uint32_t Time = Now+Ofs;
     uint8_t FLR1 = Plan.getChannel(Time, 0, 0);
     uint8_t OGN1 = Plan.getChannel(Time, 0, 1);
     uint8_t FLR2 = Plan.getChannel(Time, 1, 0);
     uint8_t OGN2 = Plan.getChannel(Time, 1, 1);
-    printf("%10d: [%02d, %02d] [%02d, %02d]\n", Time, FLR1, FLR2, OGN1, OGN2);
+    printf("%10d: [%02d, %02d] [%02d, %02d] %+2d %+2d\n", Time, FLR1, FLR2, OGN1, OGN2, (int)(OGN1-FLR1), (int)(OGN2-OGN1) );
   }
 
   return 0; }
