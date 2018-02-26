ceb393828fcd04d1376216196f8d3401760d9ddf
Pawel Jalocha <Pawel.Jalocha@gmail.com>
2018-02-26 20:07:09 +0000

    Minor fixes

--- ctrl.h
+++ ctrl.h
@@ -1,12 +1,10 @@
 #include "FreeRTOS.h"
 #include "task.h"
-// #include "queue.h"
 
 #ifdef WITH_SDLOG
 extern SemaphoreHandle_t Log_Mutex;
 void Log_Write(char Byte);
-// #else
-// inline void Log_Write(char Byte) { }
+int Log_Free(void);
 #endif
 
 #ifdef __cplusplus
